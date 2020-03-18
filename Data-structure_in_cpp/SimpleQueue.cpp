cf33a9632f2c5a4f9623885ad140e0d5f7e04ad9
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-03-18 14:02:40 -0400

    initial commit

---
+++ Data-structure_in_cpp/SimpleQueue.cpp
@@ -0,0 +1,15 @@
+//
+//  SimpleQueue.cpp 
+//  Implemetation of Simple Queue Data Structure
+//
+//  Created by Nair Saarika Bhasi on 11/21/19.
+//  Copyright © 2019 Nair Saarika Bhasi. All rights reserved.
+//
+
+#include "SimpleQueue.hpp"
+int main(){
+    QArraySimple QObject1;
+    QObject1.displayMenu();
+    return 0;
+    
+}
